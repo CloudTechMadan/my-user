f0edda7e9116aa8e8dfe5b91fca65beafb99dd3a
CloudTechMadan <madanmarikani@gmail.com>
2025-07-21 00:17:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const redirectUri = 'https://cloudtechmadan.github.io/my-user/';
 const scope = 'openid profile email employee-api/employee-access';
 const responseType = 'token id';
 
-const attendanceApi = 'https://jprbceq0dk.execute-api.us-east-1.amazonaws.com/markAttendance';
+const attendanceApi = 'https://jprbceq0dk.execute-api.us-east-1.amazonaws.com/prod/markAttendance';
 const presignUrlApi = 'https://jprbceq0dk.execute-api.us-east-1.amazonaws.com/getAttendanceImageUrl';
 
 let accessToken = null;
