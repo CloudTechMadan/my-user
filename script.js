968bd4fc27e5475408813d573eea3cb47145825d
CloudTechMadan <madanmarikani@gmail.com>
2025-07-21 02:13:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const domain = 'https://face-attendance-admin-auth.auth.us-east-1.amazoncognito.
 const clientId = '8me27q0v6uiackv03hbqoa1p3';
 const redirectUri = 'https://cloudtechmadan.github.io/my-user/';
 const scope = 'openid profile email employee-api/employee-access';
-const responseType = 'token';
+const responseType = 'token id';
 
 const attendanceApi = 'https://jprbceq0dk.execute-api.us-east-1.amazonaws.com/prod/markAttendance';
 const presignUrlApi = 'https://jprbceq0dk.execute-api.us-east-1.amazonaws.com/prod/getAttendanceImageUrl';
