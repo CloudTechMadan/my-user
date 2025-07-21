53eb0ff8a864bb2b0a280821ecf20a8d9f789141
CloudTechMadan <madanmarikani@gmail.com>
2025-07-21 11:52:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function capture() {
 function logout() {
   localStorage.removeItem('access_token');
   sessionStorage.clear();
-  const logoutUrl = `${domain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(redirectUri)}`;
-  window.location.href = "https://face-attendance-admin-auth.auth.us-east-1.amazoncognito.com/logout?client_id=8me27q0v6uiackv03hbqoa1p3&logout_uri=https%3A%2F%2Fcloudtechmadan.github.io%2Fmy-user%2F";
+  const logoutUrl = `https://face-attendance-admin-auth.auth.us-east-1.amazoncognito.com/logout?client_id=8me27q0v6uiackv03hbqoa1p3&logout_uri=https//cloudtechmadan.github.io/my-user/`;
+  window.location.href = logoutUrl;
 
 }
